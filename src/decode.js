8739f9afa9ca3cf5f785438883e2605a3c7d738a
Amit Verma <amitverma.ukind@gmail.com>
2020-02-03 15:04:08 +0530

    Loading JSON

---
+++ src/decode.js
@@ -0,0 +1,26 @@
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../resources/2020_01_21_16_17_01.json', true); 
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+           
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+
+function init() {
+    loadJSON(function (response) {
+        // Parse JSON string into object
+        var actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
+    });
+}
+
+
+
+
